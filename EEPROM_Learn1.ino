d9ec569f715f388cd09f0eab027124d124fc5abc
Ido Ran <ran.i@seebo.com>
2016-06-18 22:36:33 +0300

    Implement 1-byte read/write to overcome page-write limit

--- EEPROM_Learn1.ino
+++ EEPROM_Learn1.ino
@@ -14,9 +14,9 @@
   {
     float lat;
     float lng;
-//    uint8_t hour;
-//    uint8_t minute;
-//    uint8_t seoncd;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t seoncd;
   };
 
   int state = 0;
@@ -30,6 +30,7 @@ void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data
   Wire.write((int)(eeaddress & 0xFF));  // Address Low Byte
   Wire.write(rdata);
   Wire.endTransmission();
+  delay(10);
 }
 
 // Address is a page address, 6-bit (63). More and end will wrap around
@@ -72,7 +73,51 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
     if (Wire.available()) buffer[c] = Wire.read();
 }
 
+const unsigned int SERIALIZED_LENGTH = 8; // float + float
 
+void write_int(unsigned int address, int &i)
+{
+  byte* pb = (byte*) &i;
+  i2c_eeprom_write_byte(0x50, address + 0, pb[0]);
+  i2c_eeprom_write_byte(0x50, address + 1, pb[1]);
+}
+
+void read_int(unsigned int address, int &i)
+{
+  byte* pb = (byte*) &i;
+  pb[0] = i2c_eeprom_read_byte(0x50, address + 0);
+  pb[1] = i2c_eeprom_read_byte(0x50, address + 1);
+}
+
+void write_float(unsigned int address, float &f)
+{
+  byte* pb = (byte*) &f;
+  for (int i=0; i<4; i++)
+    i2c_eeprom_write_byte(0x50, address + i, pb[i]);
+}
+
+void read_float(unsigned int address, float &f)
+{
+  byte* pb = (byte*) &f;
+  for (int i=0; i<4; i++)
+    pb[i] = i2c_eeprom_read_byte(0x50, address + i);
+}
+
+void write_location_item(unsigned int index, struct LocationItem &loc)
+{
+  int field_addr = 2 + index * SERIALIZED_LENGTH;
+  write_float(field_addr, loc.lng);
+  field_addr += sizeof(loc.lng);
+  write_float(field_addr, loc.lat);
+}
+
+void read_location_item(unsigned int index, struct LocationItem &loc)
+{
+  int field_addr = 2 + index * SERIALIZED_LENGTH;
+  read_float(field_addr, loc.lng);
+  field_addr += sizeof(loc.lng);
+  read_float(field_addr, loc.lat);
+}
 
   void setup() 
   {
@@ -105,16 +150,7 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
     }
     if (state == 1)
     {
-      int i = 0;
-      i2c_eeprom_write_page(0x50, 0, (byte*)&i, sizeof(i));
-      
-      for (int idx = 0; idx < i; idx++)
-      {
-        int v = idx * 2 + 32000;
-        i2c_eeprom_write_page(0x50, 2 + idx*2, (byte*)&v, sizeof(v));
-      }
-      Serial.println("memory written");
-      
+
       state = 0;
     }
     if (state == 2)
@@ -122,13 +158,14 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
       Serial.print("sizeof(struct LocationItem)="); Serial.println(sizeof(struct LocationItem));
       
       int i = 0;
-      i2c_eeprom_read_buffer(0x50, 0, (byte*)&i, sizeof(i));
+      read_int(0, i);
+      //i2c_eeprom_read_buffer(0x50, 0, (byte*)&i, sizeof(i));
       Serial.print("read i="); Serial.println(i);
       
       for (int idx = 0; idx < i; idx++)
       {
         struct LocationItem v;
-        i2c_eeprom_read_buffer(0x50, 2 + idx * sizeof(struct LocationItem), (byte*)&v, sizeof(v));
+        read_location_item(idx, v);
         Serial.print("write ["); Serial.print(idx); Serial.print("]="); 
         Serial.print(v.lng); Serial.print(","); Serial.println(v.lat); //Serial.print(","); Serial.println(v.hour);
       }
@@ -146,19 +183,51 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
     {
        // add
       int i = 0;
-      i2c_eeprom_read_buffer(0x50, 0, (byte*)&i, sizeof(i));
+      read_int(0, i);
+      //i2c_eeprom_read_buffer(0x50, 0, (byte*)&i, sizeof(i));
       Serial.print("read i="); Serial.println(i);
 
       struct LocationItem v;
       v.lat = 100 + i * 4;
       v.lng = 400 + i * 4;
 //      v.hour = 50 + i;
-      i2c_eeprom_write_page(0x50, 2 + i*sizeof(struct LocationItem), (byte*)&v, sizeof(v));
+      write_location_item(i, v);
       Serial.print("write ["); Serial.print(i); Serial.print("]="); 
       Serial.print(v.lng); Serial.print(","); Serial.println(v.lat);
       
       i++;
-      i2c_eeprom_write_page(0x50, 0, (byte*)&i, sizeof(i));
+      write_int(0, i);
+      //i2c_eeprom_write_page(0x50, 0, (byte*)&i, sizeof(i));
+      
+      state = 0;
+    }
+    if (state == 5)
+    {      
+      int i = 1;
+      float f = 100 + i * 4;
+      byte* pb = (byte*)&f;
+      Serial.print("good float: "); 
+      Serial.print(pb[0]); Serial.print(",");
+      Serial.print(pb[1]); Serial.print(",");
+      Serial.print(pb[2]); Serial.print(",");
+      Serial.print(pb[3]);
+      Serial.print("="); Serial.print(f);
+      Serial.println();
+      
+      byte a,b,c,d;
+      int address = 2 + i * SERIALIZED_LENGTH;
+      address += sizeof(float);
+      pb[0] = i2c_eeprom_read_byte(0x50, address++);
+      pb[1] = i2c_eeprom_read_byte(0x50, address++);
+      pb[2] = i2c_eeprom_read_byte(0x50, address++);
+      pb[3] = i2c_eeprom_read_byte(0x50, address++);
+      Serial.print("memory float: "); 
+      Serial.print(pb[0]); Serial.print(",");
+      Serial.print(pb[1]); Serial.print(",");
+      Serial.print(pb[2]); Serial.print(",");
+      Serial.print(pb[3]);
+      Serial.print("="); Serial.print(f);
+      Serial.println();
       
       state = 0;
     }
